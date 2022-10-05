63815e171032f9adbb4b6474d81735a984366570
Glowman554 <glowman434@gmail.com>
2022-10-05 21:13:07 +0200

    updated some examples to use direct jumps

--- examples/cond_jmp.asm
+++ examples/cond_jmp.asm
@@ -1,11 +1,7 @@
-lod r0, lo(jmphere)
-lod r1, hi(jmphere)
 lod r2, 0x10
 cmpi r2, 0x10
-jeq A
+jeqi addr(jmphere)
 nop
 
 jmphere:
-	lod r0, 0xff
-	mov r1, r0
-	jmp A
+	jmpi 0xffff
--- examples/memory.asm
+++ examples/memory.asm
@@ -3,15 +3,11 @@ lod r1, hi(0x8000)
 lod r2, 0xf0
 
 wtr A, r2
-ldr r2, A
+ldr r3, A
 
-lod r0, lo(jmphere)
-lod r1, hi(jmphere)
-cmpi r2, 0xf0
-jeq A
+cmpi r3, 0xf0
+jeqi addr(jmphere)
 nop
 
 jmphere:
-	lod r0, lo(0xffff)
-	lod r1, hi(0xffff)
-	jmp A
+	jmpi 0xffff
