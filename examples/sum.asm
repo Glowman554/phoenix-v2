a2976ed5c83584e74e678994627acf0677c2bdac
Glowman554 <glowman434@gmail.com>
2022-10-06 19:53:17 +0200

    fixed sum.asm

--- examples/sum.asm
+++ examples/sum.asm
@@ -1,41 +1,41 @@
 entry:
-    lod r0, hi(0x8005)
-    lod r1, lo(0x8005)
+    lod r0, lo(0x8005)
+    lod r1, hi(0x8005)
 
     lod r2, 1
     wtr A, r2
 
-    addi r1, 1
+    addi r0, 1
     lod r2, 4
     wtr A, r2
 
-    addi r1, 1
+    addi r0, 1
     lod r2, 8
     wtr A, r2
 
-    addi r1, 1
+    addi r0, 1
     lod r2, 10
     wtr A, r2
 
-    addi r1, 1
+    addi r0, 1
     lod r2, 11
     wtr A, r2
 
-    addi r1, 1
+    addi r0, 1
     lod r2, 2
     wtr A, r2
 
-    addi r1, 1
+    addi r0, 1
     lod r2, 5
     wtr A, r2
 
-    lod r0, hi(0x8005)
+    lod r1, hi(0x8005)
     lod r2, 6 ; number of elements to loop over (-1 element)
     lod r3, 0 ; will be the result (should be 0x29)
 
 loop:
-    lod r1, lo(0x8005)
-    add r1, r2
+    lod r0, lo(0x8005)
+    add r0, r2
     ldr r4, A ; load from memory at offset
 
     add r3, r4 ; sum the values
