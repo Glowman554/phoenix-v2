e5296432a1c30c9445097942a763cdb54700646f
Glowman554 <glowman434@gmail.com>
2022-10-29 18:32:53 +0200

    added io support

--- tools/pico_emulator/main.c
+++ tools/pico_emulator/main.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include <cpu_core.h>
@@ -27,11 +28,36 @@ void cpu_write_byte(uint16_t addr, uint8_t val) {
 
 uint8_t cpu_io_read(uint16_t addr) {
 	debugf("reading byte from io at 0x%x", addr);
-	return 0x0;
+
+		switch (addr) {
+        case 0x0:
+			{
+				uint8_t val = 0;
+				for (int i = 0; i < 8; i++) {
+					val |= gpio_get(i + 8) << i;
+				}
+				return val;
+			}
+        default:
+            debugf("invalid io address %x", addr);
+            return 0x0;
+    }
+
 }
 
 void cpu_io_write(uint16_t addr, uint8_t val) {
 	debugf("writing byte 0x%x to io at 0x%x", val, addr);
+	
+	switch (addr) {
+        case 0x0:
+			for (int i = 0; i < 8; i++) {
+				gpio_put(i, (val & 1 << i) ? true : false);
+			}
+            return;
+        default:
+            debugf("invalid io address %x", addr);
+            return;
+    }
 }
 
 uint16_t current_emit = 0;
@@ -45,13 +71,26 @@ uint16_t emit(instruction_t instr) {
 int main() {
     stdio_init_all();
 
+	for (int i = 0; i < 8; i++) {
+		gpio_init(i);
+		gpio_set_dir(i, GPIO_OUT);
+	}
+
+	for (int i = 0; i < 8; i++) {
+		gpio_init(i + 8);
+		gpio_set_dir(i + 8, GPIO_IN);
+	}
+
+
     emit(LOD(R0, 0));
     emit(LOD(R1, 0));
     emit(LOD(R5, 0));
 
-    uint16_t loop = emit(NORI(R5, 0));
+    uint16_t loop = emit(ADDI(R5, 1));
     emit(OUT(A, R5));
     emit(JMPI(loop));
 
     core_run();
+
+	while (true) {}
 }
