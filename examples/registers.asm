5a0e7f4a8b980728e7a9a9bf0b398705a1b2ee01
Glowman554 <glowman434@gmail.com>
2023-01-25 09:09:33 +0100

    small bugfixes

---
+++ examples/registers.asm
@@ -0,0 +1,18 @@
+entry:
+	lod r0, 0
+	lod r1, 1
+	lod r2, 2
+	lod r3, 3
+	lod r4, 4
+	lod r5, 5
+	lod r6, 6
+	lod r7, 7
+	lod r8, 8
+	lod r9, 9
+	lod r10, 10
+	lod r11, 11
+	lod r12, 12
+	lod r13, 13
+	lod r14, 14
+	lod r15, 15
+	jmpi 0xffff
